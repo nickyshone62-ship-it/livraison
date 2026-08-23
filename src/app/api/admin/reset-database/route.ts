a5bdd94f4dc983eb4c35dbf59e335525a10ee1e8
LivraisonOuaga <admin@livraisonouaga.bf>
2026-08-23 15:25:24 +0200

    fix: correction de l'importation prisma pour build vercel

--- src/app/api/admin/reset-database/route.ts
+++ src/app/api/admin/reset-database/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
+import { db as prisma } from '@/lib/db';
 import bcrypt from 'bcryptjs';
 
 export async function POST(req: Request) {
